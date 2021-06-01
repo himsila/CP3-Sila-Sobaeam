7df3a8294d7e40b2cbb2f7841a9b754174179b6c
himsila <83618574+himsila@users.noreply.github.com>
2021-06-01 20:44:34 +0700

    Create Exercies4_Sila_S.py
    Exercise 4 from Born2Dev

---
+++ Exercies4_Sila_S.py
@@ -0,0 +1,18 @@
+'''Exercise 4 Born2Dev'''
+
+#Create variable.
+
+fltScr_FndEng = 70.0
+fltScr_GenBsn = 65.5
+fltScr_IntComSys = 71.5
+fltScr_ComPro = 70.0
+
+#Print out data.
+
+print("--- Your Score ---")
+print("Foundation English : ", fltScr_FndEng)
+print("General Business : ", fltScr_GenBsn)
+print("Introduction to Computer System : ", fltScr_IntComSys)
+print("Computer Programming : ", fltScr_ComPro)
+
+#>>> Done. <<<
